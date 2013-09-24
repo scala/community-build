0316da27b79291f4c8cbf499e39e60a5ecc88339
Antonio Cunei <antonio.cunei@typesafe.com>
2013-09-24 16:04:45 +0200

    Added dbuild runner

---
+++ dbuild-runner.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -ex
+set -o pipefail
+export LANG="en_US.UTF-8"
+export HOME="$(pwd)"
+
+if [ "$#" -ne "2" ]
+then
+  echo "Usage: $0 <dbuild-file> <dbuild-version>"
+  exit 1
+fi
+DBUILDCONFIG="$1"
+DBUILDVERSION="$2"
+
+if [ ! -f "$DBUILDCONFIG" ]
+then
+  echo "File not found: $DBUILDCONFIG"
+  exit 1
+fi
+
+echo "dbuild version: $DBUILDVERSION"
+echo "dbuild config:"
+cat "$DBUILDCONFIG"
+
+if [ ! -d "dbuild-${DBUILDVERSION}" ]
+then
+  wget "http://downloads.typesafe.com/dbuild/${DBUILDVERSION}/dbuild-${DBUILDVERSION}.tgz"
+  tar xfz "dbuild-${DBUILDVERSION}.tgz"
+  rm "dbuild-${DBUILDVERSION}.tgz"
+fi
+cd "dbuild-${DBUILDVERSION}"
+
+bin/dbuild "$DBUILDCONFIG" 2>&1 | tee dbuild.out
+set +x
+BUILD_ID="$(grep '^\[info\]  uuid = ' dbuild.out | sed -e 's/\[info\]  uuid = //')"
+echo "The repeatable UUID of this build was: ${BUILD_ID}"
