71c438d3afa8d036b328d376fb145d93a8c57a6b
Antonio Cunei <antonio.cunei@typesafe.com>
2014-03-10 20:10:36 +0100

    Use a simple env variable, rather than running egrep

--- dbuild-runner.sh
+++ dbuild-runner.sh
@@ -32,6 +32,7 @@ fi
 
 echo "dbuild-${DBUILDVERSION}/bin/dbuild" "${@}" "$DBUILDCONFIG"
 "dbuild-${DBUILDVERSION}/bin/dbuild" "${@}" "$DBUILDCONFIG" 2>&1 | tee "dbuild-${DBUILDVERSION}/dbuild.out"
+STATUS="$?"
 BUILD_ID="$(grep '^\[info\]  uuid = ' "dbuild-${DBUILDVERSION}/dbuild.out" | sed -e 's/\[info\]  uuid = //')"
 echo "The repeatable UUID of this build was: ${BUILD_ID}"
-egrep -q "The dbuild result is.*SUCCESS" "dbuild-${DBUILDVERSION}/dbuild.out"
+exit "$STATUS"
