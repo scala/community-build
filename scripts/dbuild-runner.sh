eaf0ef1f068ba723a61fa657be2135984bc791d7
Seth Tisue <seth@tisue.net>
2015-09-04 17:22:12 -0400

    update comment about resolvers

--- scripts/dbuild-runner.sh
+++ scripts/dbuild-runner.sh
@@ -52,9 +52,11 @@ then
   # work around https://github.com/typesafehub/dbuild/issues/175
   # until we can move to a newer version of dbuild that has the fix;
   # see https://github.com/scala/community-builds/issues/108 .
-  # (this isn't a problem on Jenkins because the scala-ci maven repo
-  # has everything under the sun cached; attempting to run dbuild
-  # locally was when I hit this. - ST 9/4/2015
+  # note that this only actually applies if you run dbuild with
+  # `-l` or `-r`, which tells dbuild to ignore the list of resolvers
+  # in common.conf and use its built-in list instead.  but my
+  # current understanding is that the resolver list in common.conf
+  # should be fine both for Jenkins and for local operation. - ST 9/4/2015
   sed -i.bak "s/typesafe.artifactoryonline.com/repo.typesafe.com/g"  "dbuild-${DBUILDVERSION}/bin/"*.properties
   sed -i.bak "s/scalasbt.artifactoryonline.com/repo.scala-sbt.org/g" "dbuild-${DBUILDVERSION}/bin/"*.properties
 fi
