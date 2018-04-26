8dbf83dd07c03e054480324a0c0ea6631ec5ea78
Seth Tisue <seth@tisue.net>
2018-04-26 21:30:56 +0200

    test 2.12.6 release candidate version

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ function latestNightly() {
 }
 
 # Defaults
-scala_version_default="2.12.6-bin-84112e8"
+scala_version_default="2.12.6-bin-d7ae7a8"  # 2.12.6 release candidate as of Apr 26 2018
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
