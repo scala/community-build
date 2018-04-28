d91e7688bedc9de1e730bedab8acf2e77f74f422
Seth Tisue <seth@tisue.net>
2018-04-28 15:30:32 +0200

    oops

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ function latestNightly() {
 }
 
 # Defaults
-scala_version_default="2.13.0-pre-4ed91190"  # latest as of Apr 28
+scala_version_default="2.13.0-pre-4ed9119"  # latest as of Apr 28
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
