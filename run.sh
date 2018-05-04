37e521bfb0f0fb3afa0f25f04c265a2ecdb6df63
Seth Tisue <seth@tisue.net>
2018-05-04 09:25:11 +0200

    newer 2.13 snapshot

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ function latestNightly() {
 }
 
 # Defaults
-scala_version_default="2.13.0-pre-1c56f0a"  # latest as of May 1
+scala_version_default="2.13.0-pre-abf21b9"  # latest as of May 4
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
