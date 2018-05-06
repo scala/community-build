136638d64f297f1febd36d6a9d3282ca8b2c0738
Seth Tisue <seth@tisue.net>
2018-05-06 22:41:19 +0200

    fix minor 2.12/2.13 discrepancies

--- run.sh
+++ run.sh
@@ -15,11 +15,6 @@ rm -rf target-*/extraction || true
 export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
-function latestNightly() {
-  url='https://scala-ci.typesafe.com/job/scala-2.12.x-integrate-bootstrap/lastStableBuild/artifact/jenkins.properties/*view*/'
-  curl -f -s -L $url | grep ^version= | cut -d= -f2
-}
-
 # Defaults
 scala_version_default="2.12.7-bin-689cd08"  # 1 May 2018
 scala_version="$scala_version_default"
@@ -61,8 +56,7 @@ Examples:
   version=2.12.1-bin-933bab2 ./run.sh project1
   version=2.12.1-bin-933bab2 ./run.sh project1,project2,project3
 
-If no Scala version is specified, we use the last green run from
-   https://scala-ci.typesafe.com/job/scala-2.12.x-integrate-community-build/
+If no Scala version is specified, we use whatever's hardcoded in run.sh.
 "
   echo >&2 "$@"
   exit $ex
