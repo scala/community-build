60b8341920499beebe4796cc7a074149efaeb712
Seth Tisue <seth@tisue.net>
2018-05-22 13:04:29 +0200

    newer 2.12 snapshot (a473cb3)

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.12.7-bin-689cd08"  # 1 May 2018
+scala_version_default="2.12.7-bin-a473cb3"  # 21 May 2018
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
