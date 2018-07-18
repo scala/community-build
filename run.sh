51704def2869eab5799e05fac0def54a806a9d82
Seth Tisue <seth@tisue.net>
2018-07-18 10:23:34 -0700

    2.13: newer snapshot

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.13.0-pre-99f3618"  # Jul 11
+scala_version_default="2.13.0-pre-fd820e4"  # Jul 18
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
