519e2f1dd528eae9fb1035cb230d7371d90128dc
Seth Tisue <seth@tisue.net>
2018-08-07 15:15:03 -0700

    at end of run, call out unexpected successes & failures

--- report/Report.scala
+++ report/Report.scala
@@ -5,31 +5,63 @@ object Report extends App {
 }
 
 object ClocReport {
+
+  // sometimes there's an extra "[info]" in the line, so we have to be careful
+  val Regex = """\[([^\]]+)\] (?:\[info\] )?\*\* COMMUNITY BUILD LINE COUNT: (\d+)""".r
+
   def apply(log: io.Source): Unit = {
-    // there's sometimes an extra "[info]" in the line, so we have to be careful
-    val Regex = """\[([^\]]+)\] (\[info\] )?\*\* COMMUNITY BUILD LINE COUNT: (\d+)""".r
     val results = collection.mutable.Map.empty[String, Int].withDefaultValue(0)
-    for (Regex(project, _, count) <- log.getLines)
+    for (Regex(project, count) <- log.getLines)
       results(project) += count.toInt
     println("Lines of Scala code recompiled during this run only:")
     for ((project, sum) <- results.toSeq.sortBy(_._2).reverse)
       println(f"$sum%9d $project")
     println(f"${results.values.sum}%9d TOTAL")
   }
+
 }
 
 object SuccessReport {
+
+  // sample input:
+  //   [info] Project foo-bar-baz---------------: DID NOT RUN (frob grue zorch)
+  // regex features used:
+  //   \w    = word character
+  //   ?:    = not a capturing group
+  //   (?!-) = negative lookahead -- next character is not "-"
+  val Regex = """^\[info\] Project ((?:\w|-(?!-))+)-*: (.+) \(""".r.unanchored
+
+  // empty on 2.12.x branch; all projects should be green
+  val expectedToFail = Set[String]()
+
   def apply(log: io.Source): Unit = {
     val lines = log.getLines.dropWhile(!_.contains("---==  Execution Report ==---"))
-    var success, failed, didNotRun = 0
-    for (line <- lines if line.startsWith("[info] Project "))
-      if (line.contains(": SUCCESS "))
-        success += 1
-      else if (line.contains(": FAILED "))
-        failed += 1
-      else if (line.contains(": DID NOT RUN "))
-        didNotRun += 1
+    var success, failed, didNotRun, unexpected = 0
+    var unexpectedFailures = collection.mutable.Buffer[String]()
+    for (Regex(name, status) <- lines)
+      status match {
+        case "SUCCESS" =>
+          success += 1
+          if (expectedToFail(name)) {
+            println(s"unexpected SUCCESS: $name")
+            unexpected += 1
+          }
+        case "FAILED" =>
+          failed += 1
+          if (!expectedToFail(name)) {
+            println(s"unexpected FAILED: $name")
+            unexpected += 1
+          }
+        case "DID NOT RUN" =>
+          didNotRun += 1
+          if (!expectedToFail(name)) {
+            println(s"unexpected DID NOT RUN: $name")
+            unexpected += 1
+          }
+      }
     val total = success + failed + didNotRun
     println(s"SUCCESS $success FAILED $failed DID NOT RUN $didNotRun TOTAL $total")
+    println(s"UNEXPECTED $unexpected")
   }
+
 }
