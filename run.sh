0b5a3f7ef14b15c2b6b61110dd02334696f9ff48
Seth Tisue <seth@tisue.net>
2018-09-26 10:09:58 -0700

    2.13: newer Scala SHA (includes sbt 0.13->1 upgrade)

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.13.0-pre-bcc5c7c"  # Sep 20
+scala_version_default="2.13.0-pre-8008069"  # Sep 26, includes sbt 0.13->1 upgrade
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
