01a80322b6a74d79a4181d56eb6c6200e813f050
Seth Tisue <seth@tisue.net>
2018-10-18 10:43:02 -0700

    update JDK 11 expected-failure list

--- report/Report.scala
+++ report/Report.scala
@@ -38,23 +38,19 @@ object SuccessReport {
         Set()
       case _ =>
         Set(
-          "akka",
-          "akka-stream",
-          "blaze",
-          "breeze",
-          "case-app",
+          "blaze",  // test failure (org.http4s.blaze.pipeline.stages.SSLStageSpec)
+          "breeze",  // "object istack is not a member of package com.sun"
+          "case-app",  // needs scala/bug#11125 workaround
           "coursier",  // needs scala/bug#11125 workaround
-          "fs2",
-          "github4s",
-          "kxbmap-configs",
-          "pprint",
-          "scala-debugger",
-          "scala-refactoring",
-          "scalamock",
-          "slick",
+          "fs2",  // new overloads on JDK 11 need disambiguation
+          "kxbmap-configs",  // "sjsonnew.DeserializationException: Field not found: $type"
+          "metaconfig",  // needs scala/bug#11125 workaround
+          "scala-refactoring",  // needs scala/bug#11125 workaround?
+          "scalamock",  // needs scala/bug#11125 workaround
+          "scalatex",  // scripted-plugin not found?
+          "slick",  // "object bind is not a member of package javax.xml"
           "ssl-config", // https://github.com/lightbend/ssl-config/issues/98
-          "twitter-util",
-          "zinc",
+          "twitter-util",  // "javadoc: error - invalid flag: -d"
         )
     }
 
