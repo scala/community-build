58e971161869bc2246b0a2e553f51b0da85b1cd8
Seth Tisue <seth@tisue.net>
2018-12-03 08:04:14 -0800

    github4s went green on JDK 11

--- report/Report.scala
+++ report/Report.scala
@@ -41,7 +41,6 @@ object SuccessReport {
           "akka-stream",  // "Class jdk.internal.HotSpotIntrinsicCandidate not found - continuing with a stub."
           "breeze",  // "object istack is not a member of package com.sun"
           "coursier",  // needs unfreeze to pull in https://github.com/coursier/coursier/pull/937
-          "github4s",  // "java.lang.NoClassDefFoundError: java/sql/Timestamp"
           "kxbmap-configs",  // "sjsonnew.DeserializationException: Field not found: $type"
           "sbt-util",  // needs scala/bug#11125 workaround
           "scala-debugger",  // "object FieldInfo is not a member of package sun.reflect"
