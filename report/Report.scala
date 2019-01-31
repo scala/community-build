fb2a56da1ac2e4ad2e506f41407fc165b50a88b4
Seth Tisue <seth@tisue.net>
2019-01-30 18:53:36 -0800

    update jdk 11 expected-failure list

--- report/Report.scala
+++ report/Report.scala
@@ -39,7 +39,9 @@ object SuccessReport {
       case _ =>
         Set(
           "breeze",  // "object istack is not a member of package com.sun"
+          "compiler-bridge-1.0",  // not investigated
           "coursier",  // needs unfreeze to pull in https://github.com/coursier/coursier/pull/937
+          "doobie",  // needs scala/bug#11125 workaround
           "kxbmap-configs",  // "sjsonnew.DeserializationException: Field not found: $type"
           "playframework", // not investigated yet
           "sbt-util",  // needs scala/bug#11125 workaround
