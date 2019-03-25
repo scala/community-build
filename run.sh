c784bf51816bd70161570f32b1e128caa3db7d1f
Adriaan Moors <adriaan@lightbend.com>
2019-03-25 13:13:31 +0100

    bump scala to 2.13.0-pre-54165d9

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.13.0-pre-ca57b92"  # Mar 24
+scala_version_default="2.13.0-pre-54165d9"  # Mar 25
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
