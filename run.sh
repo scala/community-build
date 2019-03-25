bc5f895b2e05842ebe98fd28e34cbb5a4542f3fa
Seth Tisue <seth@tisue.net>
2019-03-25 07:27:56 +0100

    2.13: new Scala SHA

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.13.0-pre-d7f81e5"  # Mar 22
+scala_version_default="2.13.0-pre-ca57b92"  # Mar 24
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
