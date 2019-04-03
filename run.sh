ba13c35b3355e389417456b1e09953413f8b512f
Adriaan Moors <adriaan@lightbend.com>
2019-04-03 23:38:40 +0200

    2.13.0-pre-63769af --> RC1?!?

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.13.0-pre-041fc23"  # Apr 2
+scala_version_default="2.13.0-pre-63769af"  # Apr 3 -- RC1 candidate
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
