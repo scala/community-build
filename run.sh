21d6d173077b5c2a6c33549fa730ca685d7d8982
Adriaan Moors <adriaan@lightbend.com>
2019-05-09 21:32:47 +0200

    bump to 2.13.0-pre-2ef2f9f -- RC2 candidate sha

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.13.0-pre-d73f85d"  # May 8
+scala_version_default="2.13.0-pre-2ef2f9f"  # May 9
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
