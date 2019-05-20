045da03386e27f443489324a6eda73b660fac531
Seth Tisue <seth@tisue.net>
2019-05-20 09:20:54 -0700

    haha fix merge oops

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.12.9-bin-d28a129"  # May 17
+scala_version_default="2.13.0-RC2"
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
