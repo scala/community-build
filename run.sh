8954a441b1e5eabe4c292dcaf614fabbae67ba84
Seth Tisue <seth@tisue.net>
2019-05-23 10:50:59 -0700

    fix reporting

--- run.sh
+++ run.sh
@@ -191,6 +191,6 @@ rm -rf target-*/project-builds
 
 # report summary information (line counts, green project counts, ...?)
 cd report
-sbt -error --supershell=false "run ../dbuild-${DBUILDVERSION}/dbuild.out"
+sbt -error -Dsbt.supershell=false "run ../dbuild-${DBUILDVERSION}/dbuild.out"
 
 exit $STATUS
