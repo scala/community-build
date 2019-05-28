2a3820646eb2be0631705e9dc7a27b9f7a96c46f
Adriaan Moors <adriaan@lightbend.com>
2019-05-28 19:59:52 +0200

    Bump scala to 2.11.12-bin-efd31ff

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.11.12"
+scala_version_default="2.11.12-bin-efd31ff" # May 28
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
