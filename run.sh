967eb2d96afc6cb232f438c6f9b2c3307d0cf7fd
Seth Tisue <seth@tisue.net>
2019-06-07 14:21:44 +0200

    2.13: 2.13.0

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ export LANG="en_US.UTF-8"
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.13.0-pre-43e040f"  # could this be our 2.13.0...?
+scala_version_default="2.13.0"
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
