003023dc096372fe66e986e64b4913a68c2fde67
Seth Tisue <seth@tisue.net>
2019-08-07 20:11:16 -0700

    wip

--- report/Report.scala
+++ report/Report.scala
@@ -48,6 +48,7 @@ object SuccessReport {
     "paradox",          // no 2.13 upgrade (checked Aug 6 2019)
     "scalastyle",       // no 2.13 upgrade (checked Aug 6 2019)
     "scalatest-tests",  // let's not worry about it until we move to ScalaTest 3.1
+    "tsec",             // needs ScalaTest 3.1
     "twitter-util",     // no 2.13 upgrade (checked Aug 6 2019); an unmerged third-party PR exists, we could try that?
   )
 
