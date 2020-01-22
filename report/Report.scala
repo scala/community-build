fcd658bc35ecd95011ccbcff592cf040acd4e1db
Seth Tisue <seth@tisue.net>
2020-01-21 20:32:16 -0800

    pull coursier from the build
    as per:
    
    [coursier:error] Warning: It seems you're using the default sbt launcher.
    [coursier:error] The coursier build currently requires using the coursier-based
    [coursier:error] sbt-launcher (https://github.com/coursier/sbt-launcher).
    [coursier:error] Use
    [coursier:error]   ./sbt
    [coursier:error] to run the coursier-based launcher instead.

--- report/Report.scala
+++ report/Report.scala
@@ -37,7 +37,6 @@ object SuccessReport {
   val Regex = """\[info\] Project ((?:\w|-(?!-))+)-*: ([^\(]+) \((?:stuck on broken dependencies: )?(.*)\)""".r
 
   val jdk8Failures = Set[String](
-    "coursier",           // weird git submodule problem when I tried to unfreeze to get 2.13 support. try again I guess
     "pfps-shopping-cart", // refined-related macro error: https://github.com/scala/community-build/issues/1039
   )
 
