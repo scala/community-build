558806100b28f9bdfe38a0fa70d6528a65d21f4e
Seth Tisue <seth@tisue.net>
2020-01-21 20:20:38 -0800

    2.13: pull scalafix from build
    we can reinstate if it ever makes the jump to 2.13

--- report/Report.scala
+++ report/Report.scala
@@ -39,7 +39,6 @@ object SuccessReport {
   val jdk8Failures = Set[String](
     "coursier",           // weird git submodule problem when I tried to unfreeze to get 2.13 support. try again I guess
     "pfps-shopping-cart", // refined-related macro error: https://github.com/scala/community-build/issues/1039
-    "scalafix",           // no 2.13 upgrade (checked Dec 20 2019)
   )
 
   val jdk11Failures = Set[String](
